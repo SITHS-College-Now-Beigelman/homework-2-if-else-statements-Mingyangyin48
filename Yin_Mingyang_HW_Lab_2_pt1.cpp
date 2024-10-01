0813c0aac76dfe890e07519ecd87796c1bd1952d
Mingyangyin48 <mingyangy@nycstudents.net>
2024-10-01 17:49:50 -0400

    hw 2

---
+++ Yin_Mingyang_HW_Lab_2_pt1.cpp
@@ -0,0 +1,28 @@
+// Mingyang Michelle Yin
+// Oct. 1, 2024
+// HW pt 1
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int month;
+    int day;
+
+    cout << "Please insert what month is it (1 is January and 12 is December): ";
+    cin >> month;
+
+    cout << "Please insert what day is it (1-31) : ";
+    cin >> day;
+    cout << endl;
+
+    if (3 >= month && 21 >= day || 6 <= )
+        cout << "It is spring now!";
+
+    else 
+    cout << "end";
+
+    return 0;
+}
---
+++ Yin_Mingyang_HW_Lab_2_pt2.cpp
@@ -0,0 +1,13 @@
+// Mingyang Michelle Yin
+// Oct. 1, 2024
+// HW pt 2
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+
+    return 0;
+}
